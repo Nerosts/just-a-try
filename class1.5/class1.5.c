5d95a4a0abb7b752a86aa2e8acd1dc9ca8588fa2
NeroWumu <122027647+Nerosts@users.noreply.github.com>
2023-06-16 21:05:37 +0800

    Add files via upload

---
+++ class1.5/class1.5.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//小乐乐最近接触了求和符号Σ，他想计算的结果。但是小乐乐很笨，请你帮助他解答。
+//int main() {
+//    int n = 0;
+//    long sum = 0;
+//    scanf("%d", &n);
+//    for (int i = 1; i <= n; i++)
+//    {
+//        sum += i;
+//    }
+//    printf("%ld", sum);
+//    return 0;
+//}
+
+
+//给定两个整数a和b (0 < a,b < 10,000)，计算a除以b的整数商和余数。
+//int main() {
+//    int a, b;
+//    scanf("%d %d", &a, &b);
+//    printf("%d %d", a / b, a % b);
+//    return 0;
+//}
+
+
+////问题：计算BMI指数（身体质量指数）。BMI指数（即身体质量指数，简称体质指数又称体重，英文为Body Mass Index，简称BMI），是用体重公斤数除以身高米数平方得出的数字，是目前国际上常用的衡量人体胖瘦程度以及是否健康的一个标准。主要用于统计用途
+//，当我们需要比较及分析一个人的体重对于不同高度的人所带来的健康影响时，BMI值是一个中立而可靠的指标。
+//
+//数据范围：输入的数据满足
+//50
+//≤
+//�
+//≤
+//180
+//
+//50≤n≤180
+//int main() {
+//    int weight, hight;
+//    scanf("%d %d", &weight, &hight);
+//    printf("%0.2lf", weight * 1.0 / (hight * hight * 1.0 * 0.01 * 0.01));
+//    return 0;
+//}
