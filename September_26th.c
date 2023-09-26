6ff8dc8133d198d32d10271253e825be54b8def8
NeroWumu <122027647+Nerosts@users.noreply.github.com>
2023-09-26 23:29:32 +0800

    9.26

---
+++ September_26th.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int my_strcmp(const char* arr1, const char* arr2)
+//{
+//    
+//	while (*arr1 == *arr2)
+//	{
+//		if (*arr1 == '\0')
+//		{
+//			return 0;
+//		}
+//		arr1++;
+//		arr2++;
+//	}
+//	if (*arr1 > *arr2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr1[] = "qaz";
+//	char arr2[]= "qzz";
+//	printf("%d", my_strcmp(arr1, arr2));
+//	return 0;
+//}
+
+//char* my_strcat(char* destination, char* source)
+//{
+//	char* result = destination;
+//	while (*destination)
+//	{
+//		destination++;
+//	}
+//	while (*destination++ = *source++)
+//	{
+//		;
+//	}
+//	return result;
+//}
+//
+//int main()
+//{
+//	char arr1[10] = "qaz";
+//	char arr2[]= "qzz";
+//	
+//	printf("%s", my_strcat(arr1, arr2));
+//	return 0;
+//}
