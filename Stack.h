03019f838039ee1be32d21d7db4fc5af2bd77742
NeroWumu <122027647+Nerosts@users.noreply.github.com>
2024-01-29 23:22:49 +0800

    1.29

--- Stack.h
+++ Stack.h
@@ -1,26 +1,36 @@
 #pragma once
-#include<stdio.h>
-#include<assert.h>
-#include<stdlib.h>
-#include<stdbool.h>
 
-typedef int STDataType;
-
-typedef struct Stack
+namespace MyStack
 {
-	int* a;
-	int top;		// 标识栈顶位置的
-	int capacity;
-}ST;
+	template<class T, class Container=deque<T>>
+	class stack
+	{
+	public:
+		void push(const T& x)
+		{
+			_c.push_back(x);
+		}
 
-void STInit(ST* ps);//初始化
-void STDestroy(ST* ps);//销毁
+		void pop()
+		{
+			_c.pop_back();
+		}
 
-void STPush(ST* ps, STDataType x);// 栈顶插入
-void STPop(ST* ps);// 栈顶删除
+		T& top()
+		{
+			return _c.back();
+		}
 
-STDataType STTop(ST* ps);//返回栈顶元素
+		size_t size()
+		{
+			return _c.size();
+		}
 
-bool STEmpty(ST* ps);//判断栈是否为空
-
-int STSize(ST* ps);//栈中元素数量
+		bool empty()
+		{
+			return _c.empty();
+		}
+	private:
+		Container _c;
+	};
+}
--- test.cpp
+++ test.cpp
@@ -1,11 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include"Stack.h"
+#include<vector>
+#include<deque>
 using namespace std;
-#include"vector.h"
-
 
 int main()
 {
-	MyVector::test3();
+	MyStack::stack<int,vector<int>> s1;
+	s1.push(1);
+	s1.push(2);
+	while (!s1.empty())
+	{
+		cout << s1.top() << " ";
+		s1.pop();
+	}
+	cout << endl;
 	return 0;
 }
