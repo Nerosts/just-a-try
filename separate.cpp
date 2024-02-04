e0935f1dcecd66d912ff329491161adaef52d688
NeroWumu <122027647+Nerosts@users.noreply.github.com>
2024-02-04 18:25:16 +0800

    2.4

---
+++ separate.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"separate.h"
+template<class T>
+//这里进行定义
+T Add(const T& left, const T& right)
+{
+	return left + right;
+}
+
+void func(int a, int b)
+{
+	cout << a << endl;
+}
