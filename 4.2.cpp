ef45c531a8d253cb458892873b3208c739a061f3
NeroWumu <122027647+Nerosts@users.noreply.github.com>
2024-04-02 23:56:51 +0800

    4.2

---
+++ 4.2.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+void solve()
+{
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int N = 0;
+	cin >> N;
+	while (N--)
+	{
+		solve();
+	}
+	return 0;
+}
