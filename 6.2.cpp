27c97d4d1f96fc884cbd63ea0b6e5596640c4570
NeroWumu <122027647+Nerosts@users.noreply.github.com>
2024-06-02 21:28:54 +0800

    6.2

---
+++ 6.2.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+int main()
+// {
+//     // B *obj = new B();
+//     // while (true)
+//     // {
+//     //     obj->test();
+//     //     sleep(1);
+//     // }
+
+//     srand(time(nullptr) ^ getpid() ^ pthread_self());
+//     EnableScreen(); // 开启日志显示器打印功能
+//     // EnableFile();
+//     std::unique_ptr<ThreadPool<Task>> tp = std::make_unique<ThreadPool<Task>>(5); // C++14新特性
+//     tp->InitThreadPool();
+//     tp->Start();
